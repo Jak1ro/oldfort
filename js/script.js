656a9f8aec4f778d115bdfa451c7be762faca7f4
sergiuszorgo <sergiuszorgo@gmail.com>
2024-03-14 14:56:36 +0200

    meta tagas

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 const burger = document.getElementById('burger');
 const confirm = document.getElementById('confirm');
 const bodyPage = document.querySelector('body');
+// Google Tag Manager
+
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5NN2P6RW');
 
 // HAMBURG START
 
